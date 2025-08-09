2c631e4be66332e231d866f3fd2a593a1e3df336
Catherine Wuyep <wuyepkate@gmail.com>
2025-08-09 21:35:05 +0100

    fix for profile

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { CartProvider } from '@/contexts/CartContext';
+import CartProvider from '@/contexts/CartContext';
 
 export default function App({ Component, pageProps }: AppProps) {
   // Wrap the Component with CartProvider here
